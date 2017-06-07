2c7b571c97ac15e2f718eb19a944790c7cc2b66c
Riley Bracken <rileybracken@gmail.com>
2017-06-07 13:05:44 -0600

    Adds global reset stylesheet.

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Document, {
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+import { stylesheet } from './styles.css';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+module.exports = {
+  plugins: [
+    require('postcss-cssnext')(),
+    require('postcss-modules')({
+      generateScopedName: '[local]-[hash:base64:5]',
+    }),
+    require('cssnano')(),
+  ],
+};
